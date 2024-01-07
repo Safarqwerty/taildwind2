6b87a6220f670a12511be148a49718bce2895431
Safarqwerty <safar.alam79@gmail.com>
2024-01-07 23:27:08 +0800

    fitur kembali ke atas

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header')
     const fixedNav = header.offsetTop
+    const otwTop = document.querySelector('#otw-top')
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed')
+        otwTop.classList.remove('hidden')
+        otwTop.classList.add('flex')
     } else {
         header.classList.remove('navbar-fixed')
+        otwTop.classList.remove('flex')
+        otwTop.classList.add('hidden')
     }
 }
 
